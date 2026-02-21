74138b42ee327d1638f80885999cfeb80ce4b1c1
Nart Aliti <alitinart@gmail.com>
2026-02-21 13:26:29 +0100

    Get current date on header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,12 +1,20 @@
 import { MoonIcon } from "lucide-react";
 
 export default function Header() {
+  const date = new Date();
+
   return (
     <header className="grid grid-cols-[2fr_1fr] gap-10 w-full">
       <div className="flex flex-col justify-center">
         <div>
           <h1 className="font-bold text-xl md:text-2xl">Assalamu Alakeam</h1>
-          <p className="text-gray-400">Friday, 20 Feb</p>
+          <p className="text-gray-400">
+            {date.toLocaleString("en-US", { weekday: "long" })},{" "}
+            {date.toLocaleDateString("default", {
+              day: "2-digit",
+              month: "short",
+            })}
+          </p>
         </div>
       </div>
       <div className="flex h-full items-center justify-end">
